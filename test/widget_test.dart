f4f3f2e7c60ac9898bbd95f9a72748e02151e5fd
Alpha Olomi <alphaolomi@gmail.com>
2020-03-31 04:44:31 +0300

    feat: full major update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:idea_app/main.dart';
+import 'package:ideaapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
